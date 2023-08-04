c0a4422c849c08e507d399783432815960aafb85
XPA <xpadev@gmail.com>
2023-08-04 17:47:03 +0900

    feat: email-inputs

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -6,10 +6,12 @@ import {Selector} from "@/components/selector";
 import {useState} from "react";
 import {RoomType} from "@/@types/types";
 import {PrimaryButton} from "@/components/buttons";
+import {EmailInputs} from "@/components/email-inputs/email-inputs";
 
 export default function CreateRoom() {
   const [roomType,setRoomType] = useState<RoomType>("normal")
   const [isPrivate, setIsPrivate] = useState<boolean>(true)
+  const [emails,setEmails] = useState<string[]>([]);
   return <main className={Styles.wrapper}>
     <div className={Styles.container}>
       <div>
@@ -20,6 +22,10 @@ export default function CreateRoom() {
         <p>公開設定</p>
         <Selector value={`${isPrivate}`} options={[{label:"限定公開",value:"false"},{label:"非公開",value:"true"}]} onChange={(val)=>setIsPrivate(val==="true")}/>
       </div>
+      {isPrivate && <div>
+        <p>許可対象</p>
+        <EmailInputs onChange={(val)=>setEmails(val)} emails={emails}/>
+      </div>}
       <PrimaryButton className={Styles.createButton}>
         部屋を作成
       </PrimaryButton>
