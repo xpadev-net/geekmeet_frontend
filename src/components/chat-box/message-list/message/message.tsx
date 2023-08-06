e05575fe19d4cfe903ebe64cd417ce50e77b379f
XPA <xpadev@gmail.com>
2023-08-06 12:05:30 +0900

    feat: chat style

--- src/components/chat-box/message-list/message/message.tsx
+++ src/components/chat-box/message-list/message/message.tsx
@@ -1,13 +1,14 @@
 import { MessageItem } from "@/@types/chat";
+import Styles from "./message.module.scss";
 
 type props = {
   message: MessageItem;
 };
 const Message = ({ message }: props) => {
   return (
-    <div>
-      <p>{message.name}</p>
-      <div>{message.content}</div>
+    <div className={Styles.wrapper}>
+      <p className={Styles.sender}>{message.name}</p>
+      <div className={Styles.content}>{message.content}</div>
     </div>
   );
 };
